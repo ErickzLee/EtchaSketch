50da16959018c1fca7b3f23c1c4c7ad5f2814d28
Eric <eklee13@yahoo.com>
2022-08-28 22:34:27 -0700

    Added user input the determine size of board and mouse hovering the squares changing color

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-let board = document.querySelector('.board');
-board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-board.style.gridTemplateRows = 'repeat(16, 1fr)';
+function makeBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(16, 1fr)`;
+    //Makes the board with input size
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
+}
+}
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'green';
-    board.insertAdjacentElement('beforeend', square);
+makeBoard(16);
+
+function changeSize(input) {
+    if(input >=2 && input <= 100) {
+        makeBoard(input);
+    }
+    else {
+        console.log('Pick between 2 and 100');
+    }
 }
