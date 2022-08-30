981ad00528b8dd4567a31db55466717677377f54
Eric <eklee13@yahoo.com>
2022-08-30 03:03:31 -0700

    Finished with css

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function makeBoard(size) {
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(16, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     //Makes the board with input size
     for (let i = 0; i < size * size; i++) {
         let square = document.createElement('div');
@@ -19,10 +19,13 @@ makeBoard(16);
 
 function changeSize(input) {
     if(input >=2 && input <= 100) {
+        document.querySelector('.error').style.display = 'none';
         makeBoard(input);
     }
     else {
         console.log('Pick between 2 and 100');
+        document.querySelector('.error').style.display = 'flex';
+
     }
 }
 
